6b2991074ee00452bc78b37b2bdb891e75efee8f
Kevin Patto <kevp2010@gmail.com>
2023-09-30 23:47:42 -0500

    minor bootstrap style additions & header bg change

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,2 @@
-var apiKey = "68b22ab80eda8ec6492635c3f35eaab4";
+var apiKey = "68b22ab80eda8ec6492635c3f35eaab4"; // i don't really care if this is stolen
 var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=" + apiKey;
