b597daab7b96b8a8ac670dbc9bfd5a5adc7462b4
Kevin Patto <kevp2010@gmail.com>
2023-10-03 20:00:05 -0500

    finished api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,44 @@
-var apiKey = "68b22ab80eda8ec6492635c3f35eaab4"; // i don't really care if this is stolen
-var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=" + apiKey;
+const apiUrl = "https://api.openweathermap.org/data/2.5/forecast";
+const apiKey = "68b22ab80eda8ec6492635c3f35eaab4"; // i don't really care if this is stolen
+
+// elements
+const searchInputEl = document.querySelector("#search-input");
+const searchBtnEl = document.querySelector("#search-btn");
+
+function searchCity(event) {
+    event.preventDefault()
+
+    city = searchInputEl.value.split(' ').join('_').toLowerCase(); // replace spaces with underscores for api url
+    console.log(city);
+
+    // fetch from OpenWeather
+    fetch(apiUrl + "?q=" + city + "&appid=" + apiKey)
+        .then(function (response) {
+            if (response === 404) {
+                // add error message to page here
+                console.log("bad input - city weather data not found");
+            }
+            return response.json();
+        })
+        .then(function (data) {
+            weatherData = data;
+            console.log(weatherData);
+            renderWeather();
+            renderForecast();
+            addToLocal();
+        });
+}
+
+function renderWeather() {
+    
+}
+
+function renderForecast() {
+
+}
+
+function addToLocal() {
+
+}
+
+searchBtnEl.addEventListener("click", searchCity);
